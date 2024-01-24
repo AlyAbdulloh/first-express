ad7fa1f3b0e560f0315dad91fb62b035037f0d57
AlyAbdulloh <uwongsabar12@gmail.com>
2024-01-24 09:08:02 +0700

    fix: add user model

---
+++ models/user.model.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('user', {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            field: 'id'
+          },
+          name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            field: 'name'
+          },
+          email: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            field: 'email'
+          },
+          password: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            field: 'password'
+          },
+          created_at: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+          },
+          updates_at: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+          }
+    });
+
+    return User;
+}
