c090caa7bb48a55823491c2a15753179f6eb44a4
AlyAbdulloh <uwongsabar12@gmail.com>
2024-01-24 09:24:21 +0700

    fix: get getAll data user in user controller

--- controllers/api/user.controller.js
+++ controllers/api/user.controller.js
@@ -2,3 +2,16 @@ const Validator = require('fastest-validator');
 const { user } = require('./../../models');
 const v = new Validator();
 var bcrypt = require('bcryptjs');
+
+module.exports = {
+    index: async (req, res) => {
+        try {
+            const response = await user.findAll({
+                attribute: ['id', 'name', 'email', 'created_at', 'updated_at'],
+            });
+            return res.status(200).json(response);
+        } catch (err) {
+            return res.status(500).json({message: err.message});
+        }
+    }
+}
